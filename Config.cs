0a244b82891c6d13567350b403dd2750e30d2778
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-07-30 19:10:55 +0200

    Add files via upload

--- Config.cs
+++ Config.cs
@@ -10,6 +10,7 @@ namespace InvItemWiper
     public class Config : IRocketPluginConfiguration
     {
         public bool enabled;
+        public bool Events;
         public ushort ItemID;
         public string ServerName;
         public string LevelName;
@@ -17,6 +18,7 @@ namespace InvItemWiper
         public void LoadDefaults()
         {
             enabled = true;
+            Events = true;
             ItemID = 47580;
             ServerName = "3";
             LevelName = "Retrovia";
